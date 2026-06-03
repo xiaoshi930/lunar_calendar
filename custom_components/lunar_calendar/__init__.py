c00da76aa8fbfe6119ce532b496aafbc70302aa7
xiaoshi <115949669+xiaoshi930@users.noreply.github.com>
2026-06-03 15:11:04 +0800

    Add files via upload

--- custom_components/lunar_calendar/__init__.py
+++ custom_components/lunar_calendar/__init__.py
@@ -1,11 +1,8 @@
 """农历信息集成."""
 import logging
-import os
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.const import Platform
-from homeassistant.components.http import StaticPathConfig
-from homeassistant.components.frontend import add_extra_js_url
 
 from .const import DOMAIN
 
@@ -18,8 +15,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
-    # 添加前端资源
-    await setup_lunar_calendar_card(hass)
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
@@ -31,12 +26,3 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
-
-async def setup_lunar_calendar_card(hass: HomeAssistant) -> bool:
-    lunar_calendar_card_path = '/xiaoshi-lunar-calendar-card'
-    await hass.http.async_register_static_paths([
-        StaticPathConfig(lunar_calendar_card_path, hass.config.path('custom_components/lunar_calendar/www'), False)
-    ])
-    _LOGGER.debug(f"register_static_path: {lunar_calendar_card_path + ':custom_components/lunar_calendar/www'}")
-    add_extra_js_url(hass, lunar_calendar_card_path + f"/xiaoshi-lunar-calendar-card.js")
-    return True
